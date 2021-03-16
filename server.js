7dcdaf39b7e1accbc48392dcc8aec48018b981ca
Jake <jakekippes123gmail.com>
2021-03-16 14:24:31 -0500

    required the routes in the server.js

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(logger("dev"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-require('dotenv').config()
+require("dotenv").config();
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
-    useNewUrlParser: true,
-    useUnifiedTopology:true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
